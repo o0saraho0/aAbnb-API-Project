c376cda25c034e193155844bb4c727644e410884
o0saraho0 <sarah.jiang.1229@gmail.com>
2024-10-29 21:37:09 -0700

    image enlarge

--- frontend/src/components/SpotImage/SpotImage.jsx
+++ frontend/src/components/SpotImage/SpotImage.jsx
@@ -1,36 +1,61 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadOneSpot } from "../../store/spot";
-import './SpotImage.css'
+import { useModal } from "../../context/Modal";
+import "./SpotImage.css";
 
-const SpotImage = ({spotId}) => {
-    const dispatch = useDispatch();
-    const spot = useSelector((state) => state.spots[spotId]);
+const SpotImage = ({ spotId }) => {
+  const dispatch = useDispatch();
+  const spot = useSelector((state) => state.spots[spotId]);
+  const { setModalContent } = useModal();
 
-    useEffect(() => {
-        dispatch(loadOneSpot(spotId));
-    }, [dispatch, spotId]);
+  useEffect(() => {
+    dispatch(loadOneSpot(spotId));
+  }, [dispatch, spotId]);
 
-    if (!spot || !spot.SpotImages) return null;
+  if (!spot || !spot.SpotImages) return null;
 
-    const previewImage = spot.SpotImages.filter(image => image.preview === true);
-    const smallImages = spot.SpotImages.filter(image => image.preview === false);
+  const previewImage = spot.SpotImages.find((image) => image.preview === true);
+  const smallImages = spot.SpotImages.filter(
+    (image) => image.preview === false
+  );
 
-    return (
-        <div className="spotdetail_image_container">
-            <div className="large_image image">
-                <img src={previewImage[0].url} alt={spot.name} />
-            </div>
-            {smallImages.map((image) => (
-                <div className="small_image image" key={image.id}>
-                    <img src={image.url} alt={image.id} />
-                </div>
-            ))}
+  // Modal content component to show enlarged image
+  const ImageModalContent = ({ imageUrl, altText }) => (
+    <div className="modal_image_content">
+      <img
+        src={imageUrl}
+        alt={altText}
+        style={{ width: "55vw", aspectRatio: "3/2", objectFit: "cover" }}
+      />
+    </div>
+  );
 
+  const openImageInModal = (imageUrl, altText) => {
+    setModalContent(
+      <ImageModalContent imageUrl={imageUrl} altText={altText} />
+    );
+  };
+
+  return (
+    <div className="spotdetail_image_container">
+      <div
+        className="large_image image"
+        onClick={() => openImageInModal(previewImage.url, spot.name)}
+      >
+        <img src={previewImage.url} alt={spot.name} />
+      </div>
+      {smallImages.map((image) => (
+        <div
+          className="small_image image"
+          key={image.id}
+          onClick={() => openImageInModal(image.url, `Image ${image.id}`)}
+        >
+          <img src={image.url} alt={image.id} />
         </div>
-       
-    )
-
-}
+      ))}
+    </div>
+  );
+};
 
 export default SpotImage;
